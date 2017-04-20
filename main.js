cb6b4886c73b394a1d824a45a041e870a4729e21
Matthew Phillips <matthew@matthewphillips.info>
2017-04-20 12:50:34 -0400

    decent styles

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import foo, { bar } from './foo.js';
 var works = foo() === 'foo' && bar() === 'bar';
 
 var main = document.createElement('main');
-main.textContent = works ? 'It works!' : 'Not yet :(';
+
+main.textContent = works ? 'It does!' : 'Nope :(';
 
 document.body.appendChild(main);
+document.body.classList.add(works ? 'yep' : 'nope');
--- nomodule.js
+++ nomodule.js
@@ -2,7 +2,8 @@
   // Safari doesn't support nomodule yet so check for main
   if(!document.querySelector('main')) {
     var main = document.createElement('main');
-    main.textContent = 'Not yet :(';
+    main.textContent = 'Nope :(';
     document.body.appendChild(main);
+    document.body.classList.add('nope');
   }
 })();
